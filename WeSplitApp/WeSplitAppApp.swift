081e2d6ff494721d331ec61a85beadb5e12cb8df
Milena Beicker <milenakessia87@gmail.com>
2024-06-09 19:51:15 -0300

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Milena Beicker on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
